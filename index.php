838900bec7b7ac088ff79c4d56b5d27f1380b8ab
Yago dos Santos Senhorini <yagosenhorini30@gmail.com>
2017-06-19 15:50:17 -0300

    Criação de Página para apresentação pessoal

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Início</title>
+
+<meta charset="utf-8"/>
+<meta author="Yago dos Santos Senhorini"/>
+
+<!-- Importação de CSS, Bootstrap e Ícones !-->
+<link rel="stylesheet" type="text/CSS" href="CSS/teste.css"/>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+
+<body class="corpo">
+
+<!-- Seção de início da página !-->
+<section id="home">
+<div id="nav">
+
+<!-- Barra de navegação !-->
+<ul>
+  <li><a href="index.php"><i class="fa fa-home" style="font-size:24px"></i></a></li>
+  <li style="float:right"><a href="#"><i class="fa fa-github" style="font-size:24px"></i></a></li>
+  <li style="float:right"><a href="teste.php">Contato</a></li>
+  <li style="float:right"><a href="#">Sobre</a></li>
+</ul>
+</div>
+</section>
+
+<!-- Seção de informações !-->
+<section id="info">
+
+<div class="container-fluid">
+<div class="row">
+<div class="col-md-12">
+<h3 class="whoami">Quem Sou Eu</h3>
+<p class="borda">Lorem Ipsum...</p>
+</div>
+</div>
+</div>
+
+<div class="container-fluid">
+<div class="row">
+<div class="col-md-12">
+<h3 class="alinha">Formação</h3>
+<p class="borda">Lorem Ipsum...</p>
+</div>
+</div>
+</div>
+
+<div class="container-fluid">
+<div class="row">
+<div class="col-md-12">
+<h3 class="alinha">Projetos</h3>
+<p class="borda">Lorem Ipsum...</p>
+</div>
+</div>
+</div>
+</section>
+</body>
+
+<!-- Seção do rodapé !-->
+<section id="fim">
+<footer></footer>
+</section>
+</html>
